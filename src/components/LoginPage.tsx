28297c6d1fe9c7bb64520603c05aafe44bb11304
Dejan85 <xypnotica@gmail.com>
2020-02-24 23:40:14 +0100

    working on input onchange handler

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,6 +1,32 @@
-import * as React from "react";
+import React, { useReducer } from "react";
+import {
+  initialStateInterface,
+  actionInterface,
+  reducerInterface
+} from "./types";
+
+const initialState: initialStateInterface = { username: "", password: "" };
+
+function reducer(
+  state: initialStateInterface,
+  action: actionInterface
+): reducerInterface {
+  switch (action.type) {
+    case "username":
+      return { username: action.payload };
+    case "password":
+      return { password: action.payload };
+    default:
+      return state;
+  }
+}
 
 const LoginPage = (): JSX.Element => {
+  const [state, dispatch]: [any, any] = useReducer<any>(reducer, initialState);
+
+  console.log(state.username);
+  console.log(state.password);
+
   const onSubmitHandler = (e: React.SyntheticEvent<EventTarget>): void => {
     e.preventDefault();
     console.log("radi");
@@ -11,11 +37,25 @@ const LoginPage = (): JSX.Element => {
       <form className="login-form" onSubmit={onSubmitHandler}>
         <div className="login-input-holder">
           <label className="login-input-label">Username</label>
-          <input className="login-input" placeholder="username" />
+          <input
+            className="login-input"
+            placeholder="username"
+            value={state.username}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              dispatch({ type: "username", payload: e.target.value })
+            }
+          />
         </div>
         <div className="login-input-holder">
           <label className="login-input-label">Password</label>
-          <input className="login-input" placeholder="password" />
+          <input
+            className="login-input"
+            placeholder="password"
+            value={state.password}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              dispatch({ type: "password", payload: e.target.value })
+            }
+          />
         </div>
         <button className="login-button" type="submit">
           Login
---
+++ src/components/types.ts
@@ -0,0 +1,14 @@
+export interface initialStateInterface {
+    username: string;
+    password: string;
+}
+
+export interface actionInterface {
+    type: string;
+    payload: any;
+}
+
+export interface reducerInterface {
+    username?: string;
+    password?: string;
+}
