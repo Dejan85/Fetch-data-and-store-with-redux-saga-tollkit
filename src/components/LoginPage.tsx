8418b3eab9f95791f176f5937728622498f8e576
Dejan85 <xypnotica@gmail.com>
2020-02-24 23:00:15 +0100

    add onSubmitHandler

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,17 +1,25 @@
 import * as React from "react";
 
-const LoginPage = () => {
+const LoginPage = (): JSX.Element => {
+  const onSubmitHandler = (e: any): void => {
+    e.preventDefault();
+    console.log("radi");
+  };
+
   return (
     <div className="login">
-      <form className="login-form">
+      <form className="login-form" onSubmit={onSubmitHandler}>
         <div className="login-input-holder">
           <label className="login-input-label">Username</label>
-          <input className="login-input" />
+          <input className="login-input" placeholder="username" />
         </div>
         <div className="login-input-holder">
           <label className="login-input-label">Password</label>
-          <input className="login-input" />
+          <input className="login-input" placeholder="password" />
         </div>
+        <button className="login-button" type="submit">
+          Login
+        </button>
       </form>
     </div>
   );
